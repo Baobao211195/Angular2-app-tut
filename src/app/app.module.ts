7e246af240b6d7c2b927999e15891424867caef2
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-17 20:27:56 +0700

    Commit delete material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
@@ -15,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeroesModule} from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
@@ -32,8 +31,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
-    BrowserAnimationsModule,
-    MatInputModule
   ],
   providers: [ HeroService, MessageService],
   bootstrap: [AppComponent]
