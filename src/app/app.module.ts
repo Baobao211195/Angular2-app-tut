937624fe1af9b727cc6c5b1c44d240958759b486
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-17 20:48:19 +0700

    Bai 1- lap trinh ky dieu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import {HeroService} from './hero.service';
-import { MessageComponent } from './message/message.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {HttpClientModule} from '@angular/common/http';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
-import {HeroesModule} from './heroes/heroes.module';
+
 
 @NgModule({
-  declarations: [
+  declarations: [ /*Dùng để khai báo các thành phần: components,
+   directives and pipes mà nó thuộc về Module đó*/
     AppComponent,
-  HeroesComponent,
-  HeroDetailComponent,
-  MessageComponent,
-  DashboardComponent,
-  HeroSearchComponent
 ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
   ],
-  providers: [ HeroService, MessageService],
-  bootstrap: [AppComponent]
+  providers: [ ], /*Khi làm việc với Dependency injection,
+   chúng ta cần khai báo các Services cho Injector thực hiện việc nạp các dependencies.*/
+  bootstrap: [AppComponent] /*Khi định nghĩa root Module, chúng ta cần nói cho Angular biết chương trình cần bắt đầu ở đâu*/
 })
 export class AppModule { }
