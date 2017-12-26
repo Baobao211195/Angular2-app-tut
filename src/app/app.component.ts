c30aff2d6ea611c488aac5f7d2d88a63dbff8565
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-26 21:31:36 +0700

    Bai 1- lap trinh ky dieu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
-  /*animations: "list of animations of this component",
-  changeDetection: "change detection strategy used by this component",
-  encapsulation: "style encapsulation strategy used by this component",
-  entryComponents: "list of components that are dynamically inserted into the view of this component",
-  exportAs: "name under which the component instance is exported in a template",
-  host: "map of class property to host element bindings for events, properties and attributes",
-  inputs: "list of class property names to data-bind as component inputs",
-  interpolation: "custom interpolation markers used in this component's template",
-  moduleId: "ES/CommonJS module id of the file in which this component is defined",
-  outputs: "list of class property names that expose output events that others can subscribe to",
-  providers: "list of providers available to this component and its children",
-  queries: " configure queries that can be injected into the component",
-  selector: "css selector that identifies this component in a template",
-  styleUrls: "list of urls to stylesheets to be applied to this component's view",
-  styles: "inline-defined styles to be applied to this component's view",
-  template: "inline-defined template for the view",
-  templateUrl: "url to an external file containing a template for the view",
-  viewProviders: "list of providers available to this component and its view children",*/
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
+  url: string;
+  title: string;
+  constructor() {
+  }
+
+  ngOnInit () {
+    this.url = 'http://i.a4vn.com/2013/9/26/naruto-game5-hien-ngang-buoc-vao-closed-beta-091646.jpg';
+    this.title = 'pham van oanh';
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,14 @@ import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ /*Dùng để khai báo các thành phần: components,
-   directives and pipes mà nó thuộc về Module đó*/
+  declarations: [
     AppComponent,
 ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
-  providers: [ ], /*Khi làm việc với Dependency injection,
-   chúng ta cần khai báo các Services cho Injector thực hiện việc nạp các dependencies.*/
-  bootstrap: [AppComponent] /*Khi định nghĩa root Module, chúng ta cần nói cho Angular biết chương trình cần bắt đầu ở đâu*/
+  providers: [ ],
+  bootstrap: [AppComponent] ,
 })
 export class AppModule { }
